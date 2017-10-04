393d966774e74818c6985b0e6d14ac31b7b28ee6
anne <richardson.ae@gmail.com>
2017-10-04 18:47:55 -0500

    Write specs for GuessFactory

--- spec/guess_spec.rb
+++ spec/guess_spec.rb
@@ -1,28 +1,47 @@
 require 'guess'
 
 describe GuessFactory do
+  describe '.create' do
+    let(:correct_answer) { 2 }
 
-end
+    subject { GuessFactory.create(guess, correct_answer) }
+
+    describe 'when the guess is correct' do
+      let(:guess) { 2 }
+      it('generates a CorrectGuess') { expect(subject.class).to eq(CorrectGuess) }
+    end
+
+    describe 'when the guess is too high' do
+      let(:guess) { 3 }
+      it('generates a HighGuess') { expect(subject.class).to eq(HighGuess) }
+    end
+
+    describe 'when the guess is too low' do
+      let(:guess) { 1 }
+      it('generates a LowGuess') { expect(subject.class).to eq(LowGuess) }
+    end
+  end
+end #GuessFactory
 
 
 describe BaseGuess do
 
-end
+end #BaseGuess
 
 
 describe CorrectGuess do
 
-end
+end #CorrectGuess
 
 
 describe HighGuess do
 
-end
+end #HighGuess
 
 
 describe LowGuess do
 
-end
+end #LowGuess
 
 
 
