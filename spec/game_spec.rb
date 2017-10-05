bd4984353f926ef012a6b3c152124e6a8d303ee3
anne <richardson.ae@gmail.com>
2017-10-05 09:55:45 -0500

    Practice writing tests for private methods

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,6 +3,7 @@ require 'game'
 describe Game do
   let(:game) { Game.new }
   let(:acceptable_range) { (game.range_min..game.range_max).to_a }
+  let(:correct_answer) { 2 }
 
   describe '#initialize' do
     it 'has an instance of Game' do
@@ -34,5 +35,21 @@ describe Game do
     it 'initiates game play'
   end
 
+  describe '#lost_game?' do
+    subject { game.send(:lost_game?) }
+    context 'when the user has remaining guesses' do
+      it('returns false') { expect(subject).to eq(false) }
+    end
+  end
+
+  describe '#render_loss' do
+    it 'ends the game when guesses are exceeded'
+  end
+
+  describe '#remaining_guesses?' do
+    subject { game.send(:remaining_guesses?) }
+    it('returns true if there are remaining guesses') { expect(subject).to eq(true) }
+    it 'returns false if there are no remaining guesses'
+  end
 
 end #Game
